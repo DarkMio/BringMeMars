6d34d7f337da29147e8aae8d3f4dff2f26707ebf
DarkMio <s59330@beuth-hochschule.de>
2016-10-20 14:40:31 +0200

    alright, motion tweening next.

--- static/js/gauge.js
+++ static/js/gauge.js
@@ -1,3 +1,8 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 /**
  * This is a polyfill to have requestAnimationFrame across various browsers.
  * See this gist -> https://gist.github.com/paulirish/1579671
@@ -34,7 +39,7 @@ function setupRequestAnimationFrame() {
  * An abstract class containing the most bare and basic methods to draw any kind of gauge.
  */
 var BaseGaugeDrawer = (function () {
-    function BaseGaugeDrawer(idSelector) {
+    function BaseGaugeDrawer(idSelector, configuration) {
         var element = document.getElementById(idSelector);
         if (element instanceof HTMLCanvasElement) {
             this.canvas = element;
@@ -52,69 +57,115 @@ var BaseGaugeDrawer = (function () {
             }
         }
         this.context = this.canvas.getContext("2d");
+        this.setConfiguration(configuration);
     }
+    BaseGaugeDrawer.getDefaultConfiguration = function () {
+        return new (function () {
+            function class_1() {
+                this.backgroundLineWidth = 15;
+                this.gaugeLineWidth = 15;
+                this.backgroundColor = "#f5f5f5";
+                this.gaugeColor = "#cddc39";
+                this.displayFont = false;
+                this.fontStyle = "20pt 'Roboto', 'Verdana', 'sans-serif'";
+                this.minValue = 0;
+                this.maxValue = 100;
+                this.initialValue = Math.random() * 100;
+            }
+            return class_1;
+        }());
+    };
+    BaseGaugeDrawer.prototype.setConfiguration = function (configuration) {
+        var def = BaseGaugeDrawer.getDefaultConfiguration();
+        if (configuration == null) {
+            configuration = def;
+        }
+        this.backgroundLineWidth = configuration.backgroundLineWidth || def.backgroundLineWidth;
+        this.gaugeLineWidth = configuration.gaugeLineWidth || def.gaugeLineWidth;
+        this.backgroundColor = configuration.backgroundColor || def.backgroundColor;
+        this.gaugeColor = configuration.gaugeColor || def.gaugeColor;
+        this.displayFont = configuration.displayFont || def.displayFont;
+        this.fontStyle = configuration.fontStyle || def.fontStyle;
+        this.appendText = configuration.appendText || def.appendText;
+        this.minValue = configuration.minValue || def.minValue;
+        this.maxValue = configuration.maxValue || def.maxValue;
+        this.initialValue = configuration.initialValue || this.minValue + Math.random() * this.maxValue;
+    };
     /**
-     * Let's say we draw horizontally, where east is 0*, south 90°, west 180° and north 270°
+     * Let's say we draw horizontally, where east is 0°, south 90°, west 180° and north 270°
      * Since canvas a pretty lousy on angles, we can convert it at this point.
      * @param start (number) end path degree based on description
      * @param end (number) start path degree based on description
      */
-    BaseGaugeDrawer.prototype.getAngleDeg = function (start, end) {
-        var begin = start / 360 * Math.PI;
-        return [begin, begin + end / 360 * Math.PI];
+    BaseGaugeDrawer.getAngleDeg = function (start, end) {
+        if (start > end) {
+            var temp = start;
+            start = end;
+            end = temp;
+        }
+        var stop = (end - start) / 180 * Math.PI;
+        var begin = start / 180 * Math.PI;
+        return [begin, begin + stop];
+    };
+    BaseGaugeDrawer.getAdditiveAngle = function (start, angle) {
+        var begin = start / 180 * Math.PI;
+        var stop = begin + (angle / 180) * Math.PI;
+        return [begin, stop];
+    };
+    BaseGaugeDrawer.prototype.getValueAngle = function (value) {
+        var percentage = (value - this.minValue) * 100 / this.maxValue;
+        return BaseGaugeDrawer.getAdditiveAngle(180, percentage * 1.8);
     };
     return BaseGaugeDrawer;
 }());
-var Config = (function () {
-    function Config() {
+var HalfGaugeDrawer = (function (_super) {
+    __extends(HalfGaugeDrawer, _super);
+    function HalfGaugeDrawer(idSelector, configuration) {
+        configuration = configuration || new (function () {
+            function class_2() {
+            }
+            return class_2;
+        }());
+        configuration.displayFont = true;
+        _super.call(this, idSelector, configuration);
     }
-    return Config;
-}());
-var HalfGaugeDrawer = (function () {
-    function HalfGaugeDrawer(idSelector) {
-        this.startAngle = 0;
-        var element = document.getElementById(idSelector);
-        if (element instanceof HTMLCanvasElement) {
-            this.canvas = element;
-        }
-        else {
-            var canvasElements = element.getElementsByTagName("canvas");
-            if (canvasElements.length == 1) {
-                this.canvas = canvasElements[0];
-            }
-            else if (canvasElements.length < 1) {
-                throw "No canvas elements found!";
-            }
-            else {
-                throw "More than one canvas elements found!";
-            }
-        }
-        this.context = this.canvas.getContext("2d");
-    }
-    HalfGaugeDrawer.prototype.render = function () {
+    HalfGaugeDrawer.prototype.draw = function () {
         var width = this.canvas.width / 2;
         var height = this.canvas.height * (1 - 0.05); //for padding
         this.context.lineCap = "butt";
-        this.context.fillStyle = "#f5f5f5";
-        this.context.strokeStyle = "#f5f5f5";
+        this.drawGauge(width, height);
+        this.drawMeter(width, height);
+        this.drawText(width, height);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    HalfGaugeDrawer.prototype.drawGauge = function (width, height) {
+        this.context.fillStyle = this.backgroundColor;
+        this.context.strokeStyle = this.backgroundColor;
         this.context.beginPath();
-        this.context.arc(width, height, height - 15, Math.PI, Math.PI * 2, false);
-        this.context.lineWidth = 15;
-        this.context.stroke();
-        this.context.strokeStyle = "#cddc39";
-        // this.context.fillStyle = "#cddc39";
-        this.context.beginPath();
-        this.context.arc(width, height, height - 15, Math.PI, Math.PI * 1.5, false);
-        this.context.lineWidth = 15;
-        this.context.stroke();
-        this.context.beginPath();
-        this.context.fillStyle = "#000";
-        this.context.font = "30px Verdana";
-        this.context.textAlign = "center";
-        // -1 because the font renderer smooths sometimes and looks like a bit below than this is
-        this.context.fillText("90", width, height - 1);
+        var halfArc = BaseGaugeDrawer.getAdditiveAngle(180, 180);
+        this.context.arc(width, height, height - 15, halfArc[0], halfArc[1], false);
+        this.context.lineWidth = this.backgroundLineWidth;
         this.context.stroke();
     };
+    HalfGaugeDrawer.prototype.drawMeter = function (width, height) {
+        this.context.strokeStyle = this.gaugeColor;
+        // this.context.fillStyle = "#cddc39";
+        this.context.beginPath();
+        var gaugeArc = this.getValueAngle(this.initialValue);
+        this.context.arc(width, height, height - 15, gaugeArc[0], gaugeArc[1], false);
+        this.context.lineWidth = this.gaugeLineWidth;
+        this.context.stroke();
+    };
+    HalfGaugeDrawer.prototype.drawText = function (width, height) {
+        if (!this.displayFont) {
+            return;
+        }
+        this.context.fillStyle = "#000";
+        this.context.font = this.fontStyle;
+        this.context.textAlign = "center";
+        // -1 because the font renderer smooths sometimes and looks like a bit below than this is
+        this.context.fillText(this.initialValue.toFixed(2), width, height - 1);
+    };
     return HalfGaugeDrawer;
-}());
+}(BaseGaugeDrawer));
 //# sourceMappingURL=gauge.js.map
--- static/js/gauge.ts
+++ static/js/gauge.ts
@@ -35,49 +35,19 @@ function setupRequestAnimationFrame() {
 abstract class BaseGaugeDrawer {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
-    constructor(idSelector: string) {
-        let element = document.getElementById(idSelector);
-        if(element instanceof HTMLCanvasElement) {
-            this.canvas = element;
-        } else {
-            let canvasElements = element.getElementsByTagName("canvas");
-            if (canvasElements.length == 1) {
-                this.canvas = canvasElements[0];
-            } else if (canvasElements.length < 1) {
-                throw "No canvas elements found!";
-            } else {
-                throw "More than one canvas elements found!";
-            }
-        }
-        this.context = this.canvas.getContext("2d");
-    }
 
-    /**
-     * Let's say we draw horizontally, where east is 0*, south 90°, west 180° and north 270°
-     * Since canvas a pretty lousy on angles, we can convert it at this point.
-     * @param start (number) end path degree based on description
-     * @param end (number) start path degree based on description
-     */
-    getAngleDeg(start: number, end: number): [number, number] {
-        var begin = start / 360 * Math.PI;
-        return [begin, begin + end / 360 * Math.PI]
-    }
-}
-
-class Config {
     backgroundLineWidth: number;
     backgroundColor: string;
     gaugeLineWidth: number;
     gaugeColor: string;
+    displayFont: boolean;
+    fontStyle: string;
+    appendText: string;
+    minValue: number;
+    maxValue: number;
+    initialValue: number;
 
-}
-
-
-class HalfGaugeDrawer {
-    canvas: HTMLCanvasElement;
-    context: CanvasRenderingContext2D;
-    startAngle = 0;
-    constructor(idSelector: string) {
+    constructor(idSelector: string, configuration?: any) {
         let element = document.getElementById(idSelector);
         if(element instanceof HTMLCanvasElement) {
             this.canvas = element;
@@ -92,33 +62,120 @@ class HalfGaugeDrawer {
             }
         }
         this.context = this.canvas.getContext("2d");
+        this.setConfiguration(configuration);
     }
 
-    render() {
+    static getDefaultConfiguration() {
+        return new class {
+            backgroundLineWidth = 15;
+            gaugeLineWidth = 15;
+            backgroundColor = "#f5f5f5";
+            gaugeColor = "#cddc39";
+            displayFont = false;
+            fontStyle = "20pt 'Roboto', 'Verdana', 'sans-serif'";
+            appendText: "";
+            minValue = 0;
+            maxValue = 100;
+            initialValue = Math.random() * 100;
+        }
+    }
+
+    setConfiguration(configuration?: any) {
+        var def = BaseGaugeDrawer.getDefaultConfiguration();
+        if(configuration == null) {
+            configuration = def;
+        }
+
+        this.backgroundLineWidth = configuration.backgroundLineWidth || def.backgroundLineWidth;
+        this.gaugeLineWidth = configuration.gaugeLineWidth || def.gaugeLineWidth;
+        this.backgroundColor = configuration.backgroundColor || def.backgroundColor;
+        this.gaugeColor = configuration.gaugeColor || def.gaugeColor;
+        this.displayFont = configuration.displayFont || def.displayFont;
+        this.fontStyle = configuration.fontStyle || def.fontStyle;
+        this.appendText = configuration.appendText || def.appendText;
+        this.minValue = configuration.minValue || def.minValue;
+        this.maxValue = configuration.maxValue || def.maxValue;
+        this.initialValue = configuration.initialValue || this.minValue + Math.random() * this.maxValue;
+    }
+
+    /**
+     * Let's say we draw horizontally, where east is 0°, south 90°, west 180° and north 270°
+     * Since canvas a pretty lousy on angles, we can convert it at this point.
+     * @param start (number) end path degree based on description
+     * @param end (number) start path degree based on description
+     */
+    static getAngleDeg(start: number, end: number): [number, number] {
+        if(start > end) {
+            var temp = start;
+            start = end;
+            end = temp;
+        }
+        var stop = (end - start) / 180 * Math.PI;
+        var begin = start / 180 * Math.PI;
+        return [begin, begin + stop]
+    }
+
+    static getAdditiveAngle(start: number, angle: number): [number, number] {
+        var begin = start / 180 * Math.PI;
+        var stop = begin + (angle / 180) * Math.PI;
+        return [begin, stop]
+    }
+
+    getValueAngle(value: number): [number, number] {
+        var percentage = (value - this.minValue) * 100 / this.maxValue;
+        return BaseGaugeDrawer.getAdditiveAngle(180, percentage * 1.8);
+    }
+}
+
+
+class HalfGaugeDrawer extends BaseGaugeDrawer {
+    constructor(idSelector: string, configuration?: any) {
+        configuration = configuration || new class{};
+        configuration.displayFont = true;
+        super(idSelector, configuration);
+    }
+
+    draw() {
         let width = this.canvas.width / 2;
         let height = this.canvas.height * ( 1 - 0.05); //for padding
         this.context.lineCap = "butt";
-        this.context.fillStyle = "#f5f5f5";
-        this.context.strokeStyle = "#f5f5f5";
-        this.context.beginPath();
-        this.context.arc(width, height, height - 15, Math.PI, Math.PI * 2, false);
-        this.context.lineWidth = 15;
-        this.context.stroke();
+        this.drawGauge(width, height);
+        this.drawMeter(width, height);
+        this.drawText(width, height);
 
-        this.context.strokeStyle = "#cddc39";
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+    }
+
+    private drawGauge(width: number, height: number) {
+        this.context.fillStyle = this.backgroundColor;
+        this.context.strokeStyle = this.backgroundColor;
+        this.context.beginPath();
+        var halfArc = BaseGaugeDrawer.getAdditiveAngle(180, 180);
+        this.context.arc(width, height, height - 15, halfArc[0], halfArc[1], false);
+        this.context.lineWidth = this.backgroundLineWidth;
+        this.context.stroke();
+    }
+
+    private drawMeter(width: number, height: number) {
+        this.context.strokeStyle = this.gaugeColor;
         // this.context.fillStyle = "#cddc39";
         this.context.beginPath();
-        this.context.arc(width, height, height - 15,  Math.PI, Math.PI * 1.5, false);
-        this.context.lineWidth = 15;
-        this.context.stroke();
+        var gaugeArc = this.getValueAngle(this.initialValue);
 
-        this.context.beginPath();
+        this.context.arc(width, height, height - 15,  gaugeArc[0], gaugeArc[1], false);
+        this.context.lineWidth = this.gaugeLineWidth;
+        this.context.stroke();
+    }
+
+    private drawText(width: number, height: number) {
+        if(!this.displayFont) {
+            return;
+        }
         this.context.fillStyle = "#000";
-        this.context.font = "30px Verdana";
+        this.context.font = this.fontStyle;
         this.context.textAlign = "center";
         // -1 because the font renderer smooths sometimes and looks like a bit below than this is
-        this.context.fillText("90", width, height - 1);
-        this.context.stroke();
+        this.context.fillText(this.initialValue.toFixed(2), width, height - 1);
     }
 }
 
