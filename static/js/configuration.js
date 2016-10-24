f14e75a6806c201921ea99c25d17d245dd32dd07
DarkMio <s59330@beuth-hochschule.de>
2016-10-24 08:48:08 +0200

    some refactoring

--- static/js/configuration.js
+++ static/js/configuration.js
@@ -1,9 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+    (function () {
+        'use strict';
+        var snackbarContainer = document.querySelector('#demo-toast-example');
+        var showSnackbarButton = document.querySelector('#demo-show-toast');
+        var handler = function (event) {
+            showSnackbarButton.style.backgroundColor = '';
+        };
+        showSnackbarButton.addEventListener('click', function () {
+            'use strict';
+            showSnackbarButton.style.backgroundColor = '#' +
+                Math.floor(Math.random() * 0xFFFFFF).toString(16);
+            var data = {
+                message: 'Button color changed.',
+                timeout: 2000,
+                actionHandler: handler,
+                actionText: 'Undo'
+            };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        });
+    }());
     setupFPSListener();
     function setupFPSListener() {
         var field = document.getElementById("fpsInput");
         var slider = document.getElementById("fpsSlider");
+        var timer = document.getElementById("frameTime");
         if (field instanceof HTMLInputElement && slider instanceof HTMLInputElement) {
+            var timeCalc = function () { return (1000 / slider.value).toFixed(1); };
+            timer.innerText = timeCalc();
             field.value = slider.value;
             field.addEventListener("input", function () {
                 if (slider.MaterialSlider) {
@@ -12,9 +35,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 else {
                     slider.value = this.value; // this looks shit, but it works.
                 }
+                timer.innerText = timeCalc();
             });
             slider.addEventListener("input", function () {
                 field.value = this.value;
+                timer.innerText = timeCalc();
             });
         }
     }
--- static/js/configuration.ts
+++ static/js/configuration.ts
@@ -1,24 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
+    (function() {
+        'use strict';
+        var snackbarContainer = document.querySelector('#demo-toast-example');
+        var showSnackbarButton = document.querySelector('#demo-show-toast');
+        var handler = function(event) {
+            showSnackbarButton.style.backgroundColor = '';
+        };
+        showSnackbarButton.addEventListener('click', function() {
+            'use strict';
+            showSnackbarButton.style.backgroundColor = '#' +
+                Math.floor(Math.random() * 0xFFFFFF).toString(16);
+            var data = {
+                message: 'Button color changed.',
+                timeout: 2000,
+                actionHandler: handler,
+                actionText: 'Undo'
+            };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        });
+    }());
+
     setupFPSListener();
 
 
     function setupFPSListener() {
         var field = document.getElementById("fpsInput");
         var slider = document.getElementById("fpsSlider");
-
+        var timer = document.getElementById("frameTime");
 
         if(field instanceof HTMLInputElement && slider instanceof HTMLInputElement) {
-            field.value = slider.value;
+            var timeCalc = () => {return (1000 / slider.value).toFixed(1)};
+            timer.innerText = timeCalc();
 
+            field.value = slider.value;
             field.addEventListener("input", function () {
                 if(slider.MaterialSlider) {
                     slider.MaterialSlider.change(this.value);
                 } else {
                     slider.value = this.value; // this looks shit, but it works.
                 }
+
+                timer.innerText = timeCalc();
             });
             slider.addEventListener("input", function () {
                 field.value = this.value;
+                timer.innerText = timeCalc();
             });
         }
     }
